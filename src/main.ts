28a7e4883afd0c6788016d12bd7919e8b4335ac3
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-06-01 09:26:27 +0700

    feat : add scss

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import { createPinia, PiniaVuePlugin } from "pinia";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/scss/bootstrap.scss";
+
+Vue.use(PiniaVuePlugin);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+new Vue({
+  router,
+  pinia: createPinia(),
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div>
+      <b-button variant="danger">Success</b-button>
+    </div>
+  </main>
+</template>
